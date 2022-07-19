nmokey/UTAustinInternship

commit 0712470fa5729e857865551a1cf14d7474678981

Correct FileProcessor output filename

dataset1/src/main/java/com/data1/FileProcessor.java
@@ -151,8 +151,7 @@ public class FileProcessor {
                 if (newDest.substring(1, 13).equals(seenDest.substring(1, 13))) {
                     int incremented = Integer.parseInt(newDest.substring(15))
                             + Integer.parseInt(seenDest.substring(15));
-                    seenDest = seenDest.substring(0, 14) +":"+ incremented;
-                    seenDestinations.set(i, seenDest);
+                    seenDest = seenDest.split(":")[0] +":"+ incremented;
                     break; // move onto next newDest
                 }
                 destinationCounter--;
@@ -193,7 +192,7 @@ public class FileProcessor {
         AppScreen.updateStatus("Writing file month" + month + ".csv");
         CSVWriter writer = new CSVWriter(
                 new FileWriter(desktopPath + "/" + year + "_" + month + formatDay(startDate) + "-" + month
-                        + formatDay(Integer.parseInt(days)+Integer.parseInt(startDate)+"") + ".csv"));
+                        + formatDay(Integer.parseInt(days)+Integer.parseInt(startDate)-1+"") + ".csv"));
         writingProgress = new ProgressBar("Writing csv file: ", processedData.size());
         writer.writeNext(
                 new String[] { "device_count", "origin_census_block_group", "destination", "destination_count" });

commit 24b35a7ef6a7515240f77ecbfbc9ba3d5946ac70

Fix destination_count aggregation bug again
This fix was broken in commit 0712470fa5729e857865551a1cf14d7474678981 and thus release v1.1.0 was incorrect and buggy.

dataset1/src/main/java/com/data1/FileAggregator.java
@@ -189,7 +189,7 @@ public class FileAggregator {
      * @since 1.2.0
      */
     private void addSociodemographicData(String[] row, int i) {
-        if(row[0]==null){
+        if (row[0] == null) {
             return;
         }
         int originIndex = Collections.binarySearch(sociodemographicData, row[0]);

dataset1/src/main/java/com/data1/FileProcessor.java
@@ -144,6 +144,7 @@ public class FileProcessor {
                     int incremented = Integer.parseInt(newDest.substring(15))
                             + Integer.parseInt(seenDest.substring(15));
                     seenDest = seenDest.split(":")[0] + ":" + incremented;
+                    seenDestinations.set(i, seenDest);
                     break; // move onto next newDest
                 }
                 destinationCounter--;